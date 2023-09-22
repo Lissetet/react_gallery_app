4512a88cb5f1500786d4c6a2ad14bc8511e5ed93
Liz Trejo <liz@liztrejo.dev>
2023-09-22 10:10:06 -0700

    moves from .env to config.js for storing apiKey per project instructions

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import Photo from './Photo';
 import NotFound from './NotFound';
-import { useEffect } from 'react';
 
-const apiKey = process.env.REACT_APP_FLICKR_API_KEY;
+import apiKey from '../config';
 const baseURL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}`;
 const urlParams = `&per_page=24&format=json&nojsoncallback=1`;
 
