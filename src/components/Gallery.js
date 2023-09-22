8163c015a7db12aa9469303e51d243fe0248a02c
Liz Trejo <liz@liztrejo.dev>
2023-09-22 12:41:10 -0700

    implements loading indicator

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -15,10 +15,13 @@ const Gallery = ({topic, isIndex = false}) => {
   const tag = params.tag || topic;
   const method = isIndex ? 'getRecent' : `search&tags=${tag}`;
   const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     axios.get(`${baseURL}${method}${urlParams}`)
       .then(res => setPhotos(res.data.photos.photo))
+      .then(() => setLoading(false))
       .catch(err => console.log(err));
   }, [method]);
 
@@ -27,6 +30,7 @@ const Gallery = ({topic, isIndex = false}) => {
       <h2><span>{tag || 'Recent'}</span> Photos</h2>
       <ul>
         { 
+          loading ? <h3>Loading...</h3> :
           photos.length === 0 ? <NotFound /> :
           photos.map(photo => <Photo data={photo} key={photo.id}/>)
         }
